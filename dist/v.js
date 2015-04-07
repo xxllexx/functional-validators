1e88528ae27c7ce3daeaf41b6fef41c4b86a1585
alexk <alexk@wix.com>
2015-04-07 09:52:00 +0300

    email validator, implementation of RFC 2822

--- dist/v.js
+++ dist/v.js
@@ -103,7 +103,7 @@ function getValidators() {
 
     return {
         email : function(val, message){
-            var reg = /^[\w-']+(\.[\w-']+)*@([a-zA-Z0-9]+[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*?\.[a-zA-Z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/;
+            var reg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
             return reg.test(val) || message || "invalid email";
         },
